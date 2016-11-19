4780c16d54c8519659d3d005a0b9ce54080bcac0
daniyalakhan <daniyalakhan@programmer.net>
2016-11-20 04:27:59 +0500

    Lesson 2A - Lecture 20, Modified (method: Decrement) for updating (variable: quantity) by 1 in (file: MainActivity.java), 20Nov16

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view){
         int quantity = 2;
-        quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
