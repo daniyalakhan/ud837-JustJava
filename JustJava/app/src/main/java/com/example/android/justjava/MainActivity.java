6d4092f8f09861d17c7a21066c39224bd9fe8804
daniyalakhan <daniyalakhan@programmer.net>
2016-11-20 04:08:05 +0500

    Lesson 2A - Lecture 17, Modified (methods: Increment and Decrement) for updating (variable: quantity) in (file: MainActivity.java), 20Nov16

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -38,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus[+] button is clicked.
      */
     public void increment(View view){
-        int quantity = 3;
+        int quantity = 2;
+        quantity = 3;
         display(quantity);
     }
 
@@ -46,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus[-] button is clicked.
      */
     public void decrement(View view){
-        int quantity = 1;
+        int quantity = 2;
+        quantity = 1;
         display(quantity);
     }
 
